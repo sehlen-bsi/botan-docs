48d0d40bae12a1d52da831e92362d0001ce67280
Philippe Lieser <philippe.lieser@rohde-schwarz.com>
2023-07-20 16:02:48 +0200

    Enable more modules

--- .github/scripts/ci_build.py
+++ .github/scripts/ci_build.py
@@ -97,7 +97,13 @@ def determine_flags(target, target_os, target_cc, ccache,
 
     flags += ['--werror-mode']
 
-    flags += ['--module-policy=bsi', '--enable-modules=tls12,tls13,tls_cbc,pkcs11,xts']
+    enable_modules = []
+    enable_modules += ['tls12','tls13','tls_cbc']
+    enable_modules += ['pkcs11']
+    enable_modules += ['xts']
+    enable_modules += ['kyber','kyber_90s']
+    enable_modules += ['sphincsplus_sha2','sphincsplus_shake']
+    flags += ['--module-policy=bsi', '--enable-modules=%s' % ','.join(enable_modules)]
 
     if target in ['pdf_docs']:
         flags += ['--with-doxygen', '--with-sphinx', '--with-pdf']
