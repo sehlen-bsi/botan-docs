8cc8626b959451bf48ce109df218e926dce1c1ff
Philippe Lieser <philippe.lieser@rohde-schwarz.com>
2023-07-20 17:42:23 +0200

    Add dilithium to CI

--- .github/scripts/ci_build.py
+++ .github/scripts/ci_build.py
@@ -102,6 +102,7 @@ def determine_flags(target, target_os, target_cc, ccache,
     enable_modules += ['pkcs11']
     enable_modules += ['xts']
     enable_modules += ['kyber','kyber_90s']
+    enable_modules += ['dilithium','dilithium_aes']
     enable_modules += ['sphincsplus_sha2','sphincsplus_shake']
     flags += ['--module-policy=bsi', '--enable-modules=%s' % ','.join(enable_modules)]
 
