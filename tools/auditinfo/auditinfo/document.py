a60ff847b74a03f045b4c3f6990efc9a3996d23a
Rene Meusel <rene.meusel@rohde-schwarz.com>
2023-09-21 15:17:17 +0200

    register some useful rST substitutions

---
+++ tools/auditinfo/auditinfo/document.py
@@ -0,0 +1,25 @@
+import time
+
+from collections.abc import Mapping
+
+from auditinfo.base import *
+from auditinfo.botan import *
+
+def rst_substitutions(custom_substitutions : Mapping[str, str] = {}) -> str:
+    """ An rST prolog containing a number of useful substitutions.
+
+        Example: In rST, just write |document_gitsha| to render the
+                 current git revision of repository.
+    """
+
+    substitutions = {
+        "document_gitsha": repository_gitsha(),
+        "document_gitsha_short": repository_gitsha()[:7],
+        "document_datestamp": time.strftime("%d.%m.%Y"),
+        "botan_version": botan_version(),
+        "botan_git_base_ref": botan_git_base_ref(),
+        "botan_git_ref": botan_git_ref(),
+        **custom_substitutions
+    }
+
+    return '\n'.join([f".. |{subst}| replace:: {value}" for subst, value in substitutions.items()])
