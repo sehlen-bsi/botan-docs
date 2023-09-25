b93e62aa8e48d6095499a0ad512857742e3d737c
Rene Meusel <rene.meusel@rohde-schwarz.com>
2023-09-25 14:49:15 +0200

    Helpers for auditupdate bot

--- tools/auditinfo/auditinfo/botan.py
+++ tools/auditinfo/auditinfo/botan.py
@@ -3,15 +3,37 @@ import re
 
 from auditinfo.base import repository_root
 
+def config_file_path() -> str:
+    return os.path.join(repository_root(), "config", "botan.env")
+
+def __conf_var_pattern():
+    return re.compile(r"(^[a-zA-Z_0-9]+)=(.+)\n$")
+
 def __get_from_config(env_var: str) -> str:
-    cfgpath = os.path.join(repository_root(), "config", "botan.env")
-    cfgpattern = re.compile(r"(^[a-zA-Z_0-9]+)=(.+)\n$")
-    with open(cfgpath, encoding="utf-8") as cfg:
+    cfgpattern = __conf_var_pattern()
+    with open(config_file_path(), encoding="utf-8") as cfg:
         for line in cfg.readlines():
             match = cfgpattern.match(line)
             if match and match.group(1) == env_var:
                 return match.group(2)
     raise RuntimeError(f"Did not find ${env_var} in the environment or config")
+
+def __update_config(env_var: str, new_value: str):
+    cfgpattern = __conf_var_pattern()
+    cfgfile = ""
+    with open(config_file_path(), encoding="utf-8") as read_cfg:
+        cfgfile = read_cfg.read()
+
+    with open(config_file_path(), "w", encoding="utf-8") as write_cfg:
+        for line in cfgfile.splitlines(keepends=True):
+            match = cfgpattern.match(line)
+            if not match or match.group(1) != env_var:
+                write_cfg.write(line)
+            else:
+                write_cfg.write(f"{env_var}={new_value}\n")
+
+    if env_var in os.environ:
+        os.environ[env_var] = new_value
 
 def __get_or_throw(env_var: str) -> str:
     return os.environ[env_var] if env_var in os.environ else __get_from_config(env_var)
@@ -47,3 +69,7 @@ def auditdoc_main_branch() -> str:
 def auditdoc_autoupdate_branch() -> str:
     """ The branch on the audit documentation repo managed by the nightly auto-update check """
     return __get_or_throw("AUDITDOC_AUTO_UPDATE_BRANCH")
+
+def update_botan_git_ref(new_ref: str):
+    """ Writes the configuration file and sets the new git target reference """
+    __update_config("BOTAN_REF", new_ref)
