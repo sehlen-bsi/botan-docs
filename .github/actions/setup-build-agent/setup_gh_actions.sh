f50803d238580efb535067ebd883d94ba780650b
René Meusel <rene.meusel@rohde-schwarz.com>
2025-01-14 08:09:41 +0100

    Remove hack for apt-get on Ubuntu 22.04

--- .github/actions/setup-build-agent/setup_gh_actions.sh
+++ .github/actions/setup-build-agent/setup_gh_actions.sh
@@ -31,13 +31,7 @@ setup_softhsm_macos() {
 }
 
 if type -p "apt-get"; then
-    # Hack to deal with https://github.com/actions/runner-images/issues/8659
-    sudo rm -f /etc/apt/sources.list.d/ubuntu-toolchain-r-ubuntu-test-jammy.list
-    sudo apt-get update
-    sudo apt-get install -y --allow-downgrades libc6=2.35-* libc6-dev=2.35-* libstdc++6=12.3.0-* libgcc-s1=12.3.0-*
-
-    # Normal workflow follows
-    #sudo apt-get -qq update
+    sudo apt-get -qq update
     sudo apt-get -qq install ccache
 
     setup_softhsm_and_tpm_linux
